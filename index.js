2870cf7ee7163db2a194b7ad501a6e191f1ca7dd
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2023-12-22 01:22:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+var currentUser = 'Welcome to Flatbook';
+
+var excitedWelcomeMessage = ` WELCOME TO FLATBOOK, ${currentUser} ` + '!';
+
+var welcomeMessage = ` Welcome to Flatbook, ${currentUser} `+ '!';
+
+
+const shortGreeting =`"Welcome, " ` + '!';
